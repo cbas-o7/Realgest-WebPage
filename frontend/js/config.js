f8050f7b2aac2167f4679fbc1e3bd03abbacaee2
Sebastian <erick.cortillo@gmail.com>
2025-12-02 04:23:36 -0600

    Agrega api railway

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,5 @@
 let production = true;
 
-let API_URL = production ? `https://realgest-webpage.onrender.com/api` : `http://localhost:3000/api`;
+let API_URL = production ? `realgest-webpage-production.up.railway.app/api` : `http://localhost:3000/api`;
 
 export { API_URL };
